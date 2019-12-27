aa1a692239439000a9fb5ea068348370dce05831
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-12-27 12:25:49 +0400

    Added image into view and modified textout to show inside view

--- helpers/qasm-drawer.py
+++ helpers/qasm-drawer.py
@@ -3,5 +3,7 @@ import matplotlib as mpl
 
 circuit = QuantumCircuit.from_qasm_file('/Users/tareqdandachi/github/language-qasm/test/qft.qasm')
 
-circuit.draw(output="mpl").savefig('/Users/tareqdandachi/delete/qcp.png')
+figure = circuit.draw(output="mpl")
+figure.tight_layout()
+figure.savefig('/Users/tareqdandachi/delete/qcp.png')
 print(circuit.draw())
