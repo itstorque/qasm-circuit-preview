f5941cec3c0bef72b5b66464abfdc84652e9bb12
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-12-27 18:06:20 +0400

    Better rendering + debugging statements. Introduced dynamic path

--- helpers/qasm-drawer.py
+++ helpers/qasm-drawer.py
@@ -4,19 +4,29 @@ import sys
 
 if __name__ == "__main__":
 
-    circuit = QuantumCircuit.from_qasm_file('/Users/tareqdandachi/github/language-qasm/test/qft.qasm')
+    path = sys.argv[1];
 
-    style = {}
-    dark_style = {'backgroundcolor': '#000000', 'gatecolor': '#ffffff', 'linecolor': '#ffffff', 'textcolor': '#ffffff', 'compress': True}
+    if path[-5:].lower() != "qasm":
+        print("WARNING: File Extension Not that of a QASM file [.qasm]")
 
-    figure = circuit.draw(output="mpl", style=style, plot_barriers=True, reverse_bits=False)
-    figure.tight_layout()
-    figure.savefig('/Users/tareqdandachi/delete/qcp_light.png')
+    try:
+        circuit = QuantumCircuit.from_qasm_file(path)
 
-    plt.style.use("dark_background")
+        style = {}
+        dark_style = {'backgroundcolor': '#000000', 'gatecolor': '#ffffff', 'linecolor': '#ffffff', 'textcolor': '#ffffff', 'compress': True}
 
-    figure_dark = circuit.draw(output="mpl", style=dark_style, plot_barriers=True, reverse_bits=False) #add save as button
-    figure_dark.tight_layout()
-    figure_dark.savefig('/Users/tareqdandachi/delete/qcp_dark.png')
+        figure = circuit.draw(output="mpl", style=style, plot_barriers=True, reverse_bits=False)
+        figure.tight_layout()
+        figure.savefig('/Users/tareqdandachi/delete/qcp_light.png')
 
-    print(circuit.draw())
+        plt.style.use("dark_background")
+
+        figure_dark = circuit.draw(output="mpl", style=dark_style, plot_barriers=True, reverse_bits=False) #add save as button
+        figure_dark.tight_layout()
+        figure_dark.savefig('/Users/tareqdandachi/delete/qcp_dark.png')
+
+        print(circuit.draw())
+
+    except Exception as e:
+
+        print("ERROR drawing file:", sys.argv[1] + ":\n\n", e)
