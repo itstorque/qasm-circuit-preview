dabf59e274a958ec7fbb1212a06b581ff1c65976
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-12-28 00:40:05 +0400

    Added statevector graph

--- helpers/qasm-drawer.py
+++ helpers/qasm-drawer.py
@@ -2,6 +2,11 @@ from qiskit import QuantumCircuit
 import matplotlib as plt
 import sys
 
+from qiskit import QuantumRegister, ClassicalRegister
+from qiskit import Aer, execute
+from qiskit.tools.visualization import plot_state_city
+from qiskit.providers.aer import StatevectorSimulator
+
 if __name__ == "__main__":
 
     path = sys.argv[1]
@@ -20,13 +25,21 @@ if __name__ == "__main__":
         figure.tight_layout()
         figure.savefig('/Users/tareqdandachi/delete/qcp_light.png')
 
+        simulator = Aer.get_backend('statevector_simulator')
+        result = execute(circuit, simulator).result()
+        statevector = result.get_statevector(circuit)
+
+        figure_statevector = plot_state_city(statevector, title='Bell state')
+        figure_statevector.savefig('/Users/tareqdandachi/delete/statevector_light.png')
+
         plt.style.use("dark_background")
 
         figure_dark = circuit.draw(output="mpl", style=dark_style, plot_barriers=True, reverse_bits=reverse_bits) #add save as button
         figure_dark.tight_layout()
         figure_dark.savefig('/Users/tareqdandachi/delete/qcp_dark.png')
 
-        print(circuit.draw())
+        figure_statevector = plot_state_city(statevector, title='Bell state')
+        figure_statevector.savefig('/Users/tareqdandachi/delete/statevector_dark.png')
 
     except Exception as e:
 
