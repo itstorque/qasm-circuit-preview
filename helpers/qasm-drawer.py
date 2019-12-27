46a6f47f66585ad64bf1b5d907a2b80792bdf1e4
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-12-27 17:11:19 +0400

    Added conversion between dark and light mode rendering

--- helpers/qasm-drawer.py
+++ helpers/qasm-drawer.py
@@ -6,16 +6,17 @@ if __name__ == "__main__":
 
     circuit = QuantumCircuit.from_qasm_file('/Users/tareqdandachi/github/language-qasm/test/qft.qasm')
 
-    is_dark = sys.argv[1] == "true"
-
     style = {}
     dark_style = {'backgroundcolor': '#000000', 'gatecolor': '#ffffff', 'linecolor': '#ffffff', 'textcolor': '#ffffff', 'compress': True}
 
-    if is_dark:
-        style = dark_style
-        plt.style.use('dark_background')
-
-    figure = circuit.draw(output="mpl", style=style, plot_barriers=True, reverse_bits=False) #add save as button
+    figure = circuit.draw(output="mpl", style=style, plot_barriers=True, reverse_bits=False)
     figure.tight_layout()
-    figure.savefig('/Users/tareqdandachi/delete/qcp.png')
+    figure.savefig('/Users/tareqdandachi/delete/qcp_light.png')
+
+    plt.style.use("dark_background")
+
+    figure_dark = circuit.draw(output="mpl", style=dark_style, plot_barriers=True, reverse_bits=False) #add save as button
+    figure_dark.tight_layout()
+    figure_dark.savefig('/Users/tareqdandachi/delete/qcp_dark.png')
+
     print(circuit.draw())
