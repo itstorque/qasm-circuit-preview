e7a70d5cbb6bbf3e8a6d76eaf91d53e4eb16b844
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-12-27 13:28:04 +0400

    Added circuit image and methods to toggle circuit preview theme

--- helpers/qasm-drawer.py
+++ helpers/qasm-drawer.py
@@ -1,9 +1,21 @@
 from qiskit import QuantumCircuit
-import matplotlib as mpl
+import matplotlib as plt
+import sys
 
-circuit = QuantumCircuit.from_qasm_file('/Users/tareqdandachi/github/language-qasm/test/qft.qasm')
+if __name__ == "__main__":
 
-figure = circuit.draw(output="mpl")
-figure.tight_layout()
-figure.savefig('/Users/tareqdandachi/delete/qcp.png')
-print(circuit.draw())
+    circuit = QuantumCircuit.from_qasm_file('/Users/tareqdandachi/github/language-qasm/test/qft.qasm')
+
+    is_dark = sys.argv[1] == "true"
+
+    style = {}
+    dark_style = {'backgroundcolor': '#000000', 'gatecolor': '#ffffff', 'linecolor': '#ffffff', 'textcolor': '#ffffff', 'compress': True}
+
+    if is_dark:
+        style = dark_style
+        plt.style.use('dark_background')
+
+    figure = circuit.draw(output="mpl", style=style, plot_barriers=True, reverse_bits=False) #add save as button
+    figure.tight_layout()
+    figure.savefig('/Users/tareqdandachi/delete/qcp.png')
+    print(circuit.draw())
