d211d9c073fb1996ba4f7039b68a6dc25780f5cd
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-12-28 12:00:41 +0400

    Added logs to helper, moved elements to QasmClass and migrated statevector sim to bell counts

--- helpers/qasm-drawer.py
+++ helpers/qasm-drawer.py
@@ -4,7 +4,7 @@ import sys
 
 from qiskit import QuantumRegister, ClassicalRegister
 from qiskit import Aer, execute
-from qiskit.tools.visualization import plot_state_city
+from qiskit.tools.visualization import plot_histogram
 from qiskit.providers.aer import StatevectorSimulator
 
 if __name__ == "__main__":
@@ -18,18 +18,30 @@ if __name__ == "__main__":
     try:
         circuit = QuantumCircuit.from_qasm_file(path)
 
+        print("LOG> Parsed QASM Successfully")
+
         style = {}
-        dark_style = {'backgroundcolor': '#000000', 'gatecolor': '#ffffff', 'linecolor': '#ffffff', 'textcolor': '#ffffff', 'compress': True}
+        dark_style = {'backgroundcolor': '#000000', 'linecolor': '#ffffff', 'textcolor': '#ffffff'}
 
         figure = circuit.draw(output="mpl", style=style, plot_barriers=True, reverse_bits=reverse_bits)
         figure.tight_layout()
         figure.savefig('/Users/tareqdandachi/delete/qcp_light.png')
 
-        simulator = Aer.get_backend('statevector_simulator')
-        result = execute(circuit, simulator).result()
-        statevector = result.get_statevector(circuit)
+        print("LOG> Rendered Circuit")
 
-        figure_statevector = plot_state_city(statevector, title='Bell state')
+        print("SIZE>", circuit.size())
+        print("DEPTH>", circuit.depth())
+        print("WIDTH>", circuit.width())
+        print("OP_BREAKDOWN>", circuit.count_ops())
+        print("TENSOR_FACTORS>", circuit.num_tensor_factors())
+
+        simulator = Aer.get_backend('qasm_simulator')
+        result = execute(circuit, simulator).result()
+        counts = result.get_counts(circuit)
+
+        print("LOG> Calculated Counts:", counts)
+
+        figure_statevector = plot_histogram(counts, title='Bell-State counts')
         figure_statevector.savefig('/Users/tareqdandachi/delete/statevector_light.png')
 
         plt.style.use("dark_background")
@@ -38,9 +50,9 @@ if __name__ == "__main__":
         figure_dark.tight_layout()
         figure_dark.savefig('/Users/tareqdandachi/delete/qcp_dark.png')
 
-        figure_statevector = plot_state_city(statevector, title='Bell state')
+        figure_statevector = plot_histogram(counts, title='Bell-State counts')
         figure_statevector.savefig('/Users/tareqdandachi/delete/statevector_dark.png')
 
     except Exception as e:
 
-        print("ERROR drawing file:", sys.argv[1] + ":\n\n", e)
+        print("ERROR> drawing file:", sys.argv[1] + ":\n\n", e)
